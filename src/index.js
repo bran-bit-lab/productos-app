5a78f0c8c96e41122e24c2924226c8e11553970b
gabriel <gabmat1995@gmail.com>
2021-01-29 20:39:00 -0400

    wip: trabajando

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ function createWindow() {
   const fileUrl = require('url').format({
     protocol: 'file',
     slashes: true,
-    pathname: path.join( urlAssets, '/users/users.html' )
+    pathname: path.join( urlAssets, '/login/login.html' )
   });
   
   win.loadURL( fileUrl );
