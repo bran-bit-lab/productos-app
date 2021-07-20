5e106a7829796e778d323d41516b6b6c5cb7e0a5
gabmart1995 <gabmart1995@gmail.com>
2021-07-20 15:56:28 -0700

    wip: creacion de modal de actualizacion de perfil

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,6 +1,24 @@
+
 require('bootstrap/js/dist/dropdown');  // dropdown boostrap
 
+const { remote } = require('electron');
+const { UsersController } = remote.require('./controllers/users_controller');
+const { readFileAssets } = remote.require('./util_functions/file');
+
+const { ProfileModalComponent } = require('./profile-modal/profile-modal-component');
+
+const Tooltip = require('bootstrap/js/dist/tooltip');
+const Modal = require('bootstrap/js/dist/modal');
+
 class HomeComponent {
+
+	constructor() {
+		// inicializa los tooltips
+		this.tooltips = Array.from( document.querySelectorAll('[data-bs-toggle="tooltip"]') );
+		this.tooltips = this.tooltips.map(( element ) => new Tooltip( element ) );
+
+		this.initCards();
+	}
 
 	openUsers() {
 		return redirectTo('../users/users.html');
@@ -37,19 +55,20 @@ class HomeComponent {
 			hideElement( document.querySelector('#estadistics') );
 		}
 	}
+
+	initCards() {
+
+		const values = [ 50, 100, 80, 25 ];
+
+		const elementsDOM = document.querySelectorAll('span');
+
+		for ( let i = 0; i < elementsDOM.length; i++ ) {
+			elementsDOM[i].innerText = values[i];
+		}
+
+		this.showOptions();
+	}
 }
 
 const homeComponent = new HomeComponent();
-
-document.addEventListener('DOMContentLoaded', () => {
-
-	const values = [ 50, 100, 80, 25 ];
-
-	const elementsDOM = document.querySelectorAll('span');
-
-	for ( let i = 0; i < elementsDOM.length; i++ ) {
-		elementsDOM[i].innerText = values[i];
-	}
-
-	homeComponent.showOptions();
-});
+const profileModalComponent = new ProfileModalComponent();
