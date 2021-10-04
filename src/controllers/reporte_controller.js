622a3c6f042dee9214145aaec2acbef36d8c0486
Gabriel Martinez <gabmat1995@gmail.com>
2021-10-04 08:38:39 -0400

    wip: doc de report controller

--- src/controllers/reporte_controller.js
+++ src/controllers/reporte_controller.js
@@ -8,16 +8,26 @@ class ReporteController {
 	/** @type {?Database} */
 	databaseInstance = null;
 
+	/** Propiedad get database retorna una nueva instancia de la clase Database */
 	static get database() {
 		return this.databaseInstance || ( this.databaseInstance = new Database() );
 	}
 
+	/**
+	 * Busca la totalidad de notas por categoria
+	 *
+	 * @param  {?Object} periodo periodo de búsqueda
+	 * @param {string} periodo.from fecha de incio de búsqueda
+	 * @param {string} periodo.to fecha de final de búsqueda
+	 * @returns {Promise<(ResponseReport | null)>}
+	 * Retorna una promesa con un objeto que contiene los valores preparados para el modelo y la tabla. Si no hay registros devuelve null.
+	 */
 	static buscarNotasCategoria( periodo = null ){
 
 		return new Promise( ( resolve, reject ) => {
 			let sql = periodo ? CRUD.ObtenerTotalNotasPorCategoriaPeriodo : CRUD.ObtenerTotalNotasPorCategoriaGeneral;
 
-			this.database.consult( sql, periodo, ( error , resultados ) => {
+			this.database.consult( sql, periodo, ( error, resultados ) => {
 
 				if ( error ) {
 
@@ -60,6 +70,14 @@ class ReporteController {
 		});
 	}
 }
+
+/**
+ * ResponseReport
+ * @typedef {Object} ResponseReport
+ * @property {Array<string>} keys  listado de los nombres usados en el modelo estaditico
+ * @property {Array<number>} values listado de valores usados en el modelo estaditico
+ * @property {Array<Object>} results listado de la consulta para generar la tabla
+ */
 
 module.exports = {
 	ReporteController
